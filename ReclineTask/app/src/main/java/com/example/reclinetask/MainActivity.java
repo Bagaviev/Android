b5e1c133ce11de75d7441c44bb687d0ff6016f3f
bagavievbulat <buba1219@yandex.ru>
2021-07-18 19:19:26 +0300

    colorize

--- ReclineTask/app/src/main/java/com/example/reclinetask/MainActivity.java
+++ ReclineTask/app/src/main/java/com/example/reclinetask/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.reclinetask;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -9,35 +7,37 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Random;
 
 // todo more beautiful win/lose msg
 // todo buttons and change color app
-// todo icon
 // todo score
-// todo add win combination backlight
+
 // todo pack button style to theme
 // todo add sound
-// todo add ads
 
 // todo other tasks
 
 public class MainActivity extends AppCompatActivity {
-    static final int[] buttonIds = new int[] { R.id.b0, R.id.b1, R.id.b2, R.id.b3, R.id.b4, R.id.b5, R.id.b6, R.id.b7, R.id.b8};
-    static String LOSE_MESSAGE = "You lose, try again!";
-    static String WIN_MESSAGE = "You win!";
-    static String DRAW_MESSAGE = "It's a draw!";
-    static final int DOTS_TO_WIN = 3;
-    static final int MAX_MOVE_CNT = 9;
-    static String PLAYER_SYM = "X";
-    static String AI_SYM = "O";
-    static String EMPTY = "";
-    static int MOVE_CNT;
+    private final int[] buttonIds = new int[] { R.id.b0, R.id.b1, R.id.b2, R.id.b3, R.id.b4, R.id.b5, R.id.b6, R.id.b7, R.id.b8};
+    private int[][] winRow;
+
+    private static String LOSE_MESSAGE = "You lose, try again!";
+    private static String WIN_MESSAGE = "You win!";
+    private static String DRAW_MESSAGE = "It's a draw!";
+    private static final int DOTS_TO_WIN = 3;
+    private static final int MAX_MOVE_CNT = 9;
+    private static String PLAYER_SYM = "X";
+    private static String AI_SYM = "O";
+    private static String EMPTY = "";
+    private static int MOVE_CNT;
 
     private Button[][] buttons = new Button[3][3];
-    TableLayout layout;
-    TextView textView;
-    Button startStop;
+    private TableLayout layout;
+    private TextView textView;
+    private Button startStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < buttons.length; i++) {
             for (int j = 0; j < buttons[i].length; j++) {
                 buttons[i][j].setText(EMPTY);
+                buttons[i][j].setBackgroundColor(Color.parseColor("#FF6200EE"));
                 textView.setText(EMPTY);
                 textView.setBackgroundColor(Color.parseColor("#EAEAEA"));
                 MOVE_CNT = 0;
@@ -106,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
             if (checkWin(PLAYER_SYM)) {
                 freezeMap();
                 printMessage(WIN_MESSAGE);
+                colorize(PLAYER_SYM);
                 return;
             }
             doAiMove();
@@ -130,8 +132,9 @@ public class MainActivity extends AppCompatActivity {
                 MOVE_CNT++;
 
                 if (checkWin(AI_SYM)) {
-                    printMessage(LOSE_MESSAGE);
                     freezeMap();
+                    printMessage(LOSE_MESSAGE);
+                    colorize(AI_SYM);
                 }
 
                 break;
@@ -141,22 +144,100 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean checkWin(String symb) {
         int winToD1 = 0,  winToD2 = 0;
+        winRow = new int[DOTS_TO_WIN][DOTS_TO_WIN];
 
         for (int i = 0; i < buttons.length; i++) {
             int winToCol = 0, winToRow = 0;
 
             for (int j = 0; j < buttons[i].length; j++) {
-                if (buttons[i][j].getText() == symb) winToRow++;
-                if (buttons[j][i].getText() == symb) winToCol++;
+                if (buttons[i][j].getText() == symb)
+                    winToRow++;
+
+                if (buttons[j][i].getText() == symb)
+                    winToCol++;
             }
 
             if (winToRow == DOTS_TO_WIN || winToCol == DOTS_TO_WIN) return true;
 
-            if (buttons[i][i].getText()  == symb) winToD1++;
-            if (buttons[i][buttons[i].length-i-1].getText() == symb) winToD2++;
+            if (buttons[i][i].getText() == symb)
+                winToD1++;
+
+            if (buttons[i][buttons[i].length-i-1].getText() == symb)
+                winToD2++;
         }
 
-        if (winToD1 == DOTS_TO_WIN || winToD2 == DOTS_TO_WIN) return  true;
+        if (winToD1 == DOTS_TO_WIN || winToD2 == DOTS_TO_WIN) return true;
         return false;
+    }
+
+    private String countWinStreak(String sym) {
+        String winStreak;
+
+
+
+        if (buttons[0][0].getText().equals(sym) & buttons[0][1].getText().equals(sym) & buttons[0][2].getText().equals(sym))
+            winStreak = "h0";
+        else if (buttons[1][0].getText().equals(sym) & buttons[1][1].getText().equals(sym) & buttons[1][2].getText().equals(sym))
+            winStreak = "h1";
+        else if (buttons[2][0].getText().equals(sym) & buttons[2][1].getText().equals(sym) & buttons[2][2].getText().equals(sym))
+            winStreak = "h2";
+        else if (buttons[0][0].getText().equals(sym) & buttons[1][0].getText().equals(sym) & buttons[2][0].getText().equals(sym))
+            winStreak = "v0";
+        else if (buttons[0][1].getText().equals(sym) & buttons[1][1].getText().equals(sym) & buttons[2][1].getText().equals(sym))
+            winStreak = "v1";
+        else if (buttons[0][2].getText().equals(sym) & buttons[1][2].getText().equals(sym) & buttons[2][2].getText().equals(sym))
+            winStreak = "v2";
+        else if (buttons[0][0].getText().equals(sym) & buttons[1][1].getText().equals(sym) & buttons[2][2].getText().equals(sym))
+            winStreak = "d1";
+        else
+            winStreak = "d2";
+
+        return winStreak;
+    }
+
+    private void iterator(int i, int j) {
+        if (j == 3) {
+            for (int k = 0; k < j; k++)
+                buttons[i][k].setBackgroundColor(Color.parseColor("#ffe08a"));
+        } else if (i == 3) {
+            for (int k = 0; k < i; k++)
+                buttons[k][j].setBackgroundColor(Color.parseColor("#ffe08a"));
+        } else if (i == j) {
+            for (int k = 0; k <= i; k++)
+                buttons[k][k].setBackgroundColor(Color.parseColor("#ffe08a"));
+        } else {
+            for (int k = 0, m = j; k <= j & m >= 0; k++, m--)
+                buttons[k][m].setBackgroundColor(Color.parseColor("#ffe08a"));
+        }
+
+    }
+
+    private void colorize(String sym) {
+        switch (countWinStreak(sym)) {
+            case "h0":
+                iterator(0, 3);
+                break;
+            case "h1":
+                iterator(1, 3);
+                break;
+            case "h2":
+                iterator(2, 3);
+                break;
+            case "v0":
+                iterator(3, 0);
+                break;
+            case "v1":
+                iterator(3, 1);
+                break;
+            case "v2":
+                iterator(3, 2);
+                break;
+            case "d1":
+                iterator(2, 2);
+                break;
+            case "d2":
+                iterator(0, 2);
+                break;
+        }
     }
 }
