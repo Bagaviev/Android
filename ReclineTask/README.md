bd505d35d2b665813debfac25b9261faa60a4508
bagavievbulat <buba1219@yandex.ru>
2021-07-20 16:34:34 +0300

    Final app screenshots + project name change

---
+++ ReclineTask/README.md
@@ -0,0 +1,14 @@
+Тестовое задание в recline.xyz
+
+1) Firebase CM
+	Тулза для отправки push notification на девайсы, работает через раз, настраивается легко
+
+2) OneSignal
+	Похожий инструмент, как первый, но работает стабильнее, настраивается тяжело
+
+3) Simple Game creation experience
+
+Резюме: узнал много нового, реализовать смог все обязательные пункты.
+
+<a href="url"><img src="https://github.com/Bagaviev/Android/blob/master/ReclineTask/1.jpg" align="left" height="500" width="250" ></a>
+<a href="url"><img src="https://github.com/Bagaviev/Android/blob/master/ReclineTask/2.jpg" align="left" height="500" width="250" ></a>
---
+++ ReclineTask/app/src/main/java/com/example/reclinetask/WebViewActivity.java
@@ -0,0 +1,70 @@
+package com.example.reclinetask;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        progressBar = findViewById(R.id.progressBar);
+        drawPage();
+    }
+
+    private void drawPage() {
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new CustomWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://google.com");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {     // назад по иерархии открытых страниц
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public class CustomWebViewClient extends WebViewClient {    // норм тема рабочая
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            webView.setVisibility(View.GONE);
+            progressBar.setVisibility(View.VISIBLE);
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            progressBar.setVisibility(View.GONE);
+            webView.setVisibility(View.VISIBLE);
+            super.onPageFinished(view, url);
+        }
+    }
+}
