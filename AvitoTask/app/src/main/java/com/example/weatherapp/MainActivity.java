9ec0dfaf0d19b3a6f3f9c252721e754c38294f3e
bagavievbulat <buba1219@yandex.ru>
2021-09-09 19:29:10 +0300

    Fifth steps: get city by GPS done, passed to the API request, ok. Last step before design - create and pass search string into the request too (for manual city selection from list).

--- AvitoTask/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ AvitoTask/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.weatherapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.provider.Settings;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.weatherapp.network.NetworkService;
 import com.example.weatherapp.pojo.City;
@@ -22,13 +30,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -44,21 +47,25 @@ public class MainActivity extends AppCompatActivity {
     List<OurWeather> weatherList = new ArrayList<>();
     List<City> cityList = new ArrayList<>();
 
-    Button buttonDist;
     Button buttonWeather;
     Button buttonCity;
-    Button buttonMatch;
-    TextView textViewDist;
+    Button buttonLocate;
     TextView textViewWeather;
     TextView textViewCity;
-    TextView textViewMatch;
+    TextView textViewLocate;
+
+    static Double curLat;       // most logic holder variable
+    static Double curLon;       // most logic holder variable
 
     double lat1 = 55.693702;
     double lon1 = 37.658820;
     double lat2 = 56.102816;
     double lon2 = 40.379595;
 
-    float result;
+    private final int GPS_PERMISSION_CODE = 1;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location locationObj;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -67,33 +74,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         networkService = NetworkService.getInstance();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        buttonDist = findViewById(R.id.buttonDist);
         buttonWeather = findViewById(R.id.buttonWeather);
         buttonCity = findViewById(R.id.buttonCity);
-        buttonMatch = findViewById(R.id.buttonMatch);
-        textViewDist = findViewById(R.id.textViewDist);
+        buttonLocate = findViewById(R.id.buttonLocate);
         textViewWeather = findViewById(R.id.textViewWeather);
         textViewCity = findViewById(R.id.textViewCity);
-        textViewMatch = findViewById(R.id.textViewMatch);
+        textViewLocate = findViewById(R.id.textViewLocate);
 
         readCsvFromRaw();
 
-        buttonDist.setOnClickListener(v -> {
-            result = calcDistance(lat1, lon1, lat2, lon2);
-            textViewDist.setText(String.valueOf(result));
-        });
-
-        buttonWeather.setOnClickListener(v -> request(25.964051, 18.322344));
+        buttonWeather.setOnClickListener(v -> {
+                if (curLat != null & curLon != null)
+                    request(curLat, curLon);
+                else
+                    textViewWeather.setText("Locations not set!");
+            }
+        );
 
         buttonCity.setOnClickListener(v -> {
             List<City> list = findCity("kazan");
             textViewCity.setText(list.toString());
         });
 
-        buttonMatch.setOnClickListener(v -> {
-            String match = getClosestCity(65.998563, 57.635634);
-            textViewMatch.setText(match.toString());
+        buttonLocate.setOnClickListener(v -> {
+            handleGPS();
         });
     }
 
@@ -119,12 +125,12 @@ public class MainActivity extends AppCompatActivity {
                         String descr = item.getWeather().get(0).getDescription().toLowerCase();
 
                         OurWeather day = new OurWeather(
-                       date.substring(0, 1).toUpperCase() + date.substring(1),
-                            String.format("%.0f", item.getTemp().getDay()),
-                            String.format("%.0f", item.getPressure() / 1.333),
-                            item.getHumidity(),
-                            String.format("%.0f", item.getWindSpeed()),
-                 descr.substring(0, 1).toUpperCase() + descr.substring(1));
+                                date.substring(0, 1).toUpperCase() + date.substring(1),
+                                String.format("%.0f", item.getTemp().getDay()),
+                                String.format("%.0f", item.getPressure() / 1.333),
+                                item.getHumidity(),
+                                String.format("%.0f", item.getWindSpeed()),
+                                descr.substring(0, 1).toUpperCase() + descr.substring(1));
                         weatherList.add(day);
                     }
                     textViewWeather.setText(weatherList.toString());
@@ -139,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public String getClosestCity(double lat, double lon) {
+    public City getClosestCity(double lat, double lon) {
         Map<Float, City> map = new TreeMap<>();
 
         for (int i = 0; i < cityList.size(); i++) {
@@ -152,19 +158,7 @@ public class MainActivity extends AppCompatActivity {
                 .findFirst()
                 .get();
 
-        return actualValue.getKey().toString() + " " + actualValue.getValue().toString();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-
-        Map<K, V> result = new LinkedHashMap<>();
-        for (Map.Entry<K, V> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
+        return actualValue.getValue();
     }
 
     public List<City> findCity(String cityName) {       // sqlite чето работал херово, обходились как могли
@@ -205,5 +199,80 @@ public class MainActivity extends AppCompatActivity {
         locB.setLatitude(lat2);
         locB.setLongitude(lon2);
         return locA.distanceTo(locB);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void handleGPS() {
+        locationListener = new LocationListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                updLocation(location);
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+                textViewLocate.setText("GPS not enabled");
+            }
+        };
+
+        if (checkPermission()) {
+            if (!gpsStatusCheck())
+                startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+            locationObj = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        } else {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, GPS_PERMISSION_CODE);
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case GPS_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        if (!gpsStatusCheck())
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+                        locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+                        locationObj = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+
+                    } catch (SecurityException e) {
+                        textViewLocate.setText(e.getMessage());
+                    }
+
+                    updLocation(locationObj);
+
+                } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    Toast.makeText(this, "We need that permission", Toast.LENGTH_LONG).show();
+                } else {
+                    textViewLocate.setText("Not working without permission");
+                }
+            }
+            return;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void updLocation(Location location) {    // core method
+        City match = getClosestCity(location.getLatitude(), location.getLongitude());
+        curLat = match.getLat();
+        curLon = match.getLon();
+        textViewLocate.setText(match.getName() + ", " + match.getCountry());
+    }
+
+    public boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public boolean gpsStatusCheck() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 }
